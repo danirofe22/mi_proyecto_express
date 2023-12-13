08ca70638c5adc788294ef32bf4e58601f5e3159
danirofe <daniel.rodriguezfer@keifitech.com>
2023-12-13 12:56:57 +0100

    Feat: Base de datos implementada pero fallando y user model

---
+++ estado.md
@@ -0,0 +1,34 @@
+# Estado del Proyecto
+
+En este archivo se registrará el estado actual del proyecto.
+
+## Descripción
+
+El proyecto "miProyectoExpress" es una aplicación web desarrollada utilizando el framework Express de Node.js. El objetivo principal del proyecto es crear un servidor web que pueda manejar peticiones HTTP y proporcionar respuestas adecuadas.
+
+## Tareas Pendientes
+
+- Implementar la funcionalidad de registro de usuarios.
+- Crear las rutas y controladores para las diferentes páginas del sitio web.
+- Diseñar y desarrollar la interfaz de usuario.
+- Realizar pruebas exhaustivas para garantizar la estabilidad y funcionalidad del servidor.
+
+## Tareas Completadas
+
+- Configuración inicial del proyecto.
+- Creación de la estructura de carpetas y archivos.
+- Implementación de la funcionalidad básica del servidor.
+- Conexión a la base de datos.
+
+## Próximos Pasos
+
+- Finalizar la implementación de la funcionalidad de registro de usuarios.
+- Desarrollar las rutas y controladores para las páginas restantes.
+- Mejorar la interfaz de usuario y agregar estilos.
+- Realizar pruebas de rendimiento y optimización del servidor.
+
+
+## Fallos en ultimo estado 
+
+- Da un error al guardar los usuarios en la base de datos 
+- Base de datos en Atlas, mongoDB el usuario es el gmail clasico
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const axios = require('axios');
+const User = require('./src/models/user.model.js');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = 3000;
+
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
+const uri = process.env.MONGODB_URI;
+
+console.log(uri);
 
 app.get('/data', async (req, res) => {
     try {
@@ -16,3 +26,61 @@ app.get('/data', async (req, res) => {
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // Create new users
+        const user1 = new User({
+            username: 'JohnDoe',
+            email: 'john.doe@example.com',
+            password: 'password1'
+        });
+
+        const user2 = new User({
+            username: 'JaneSmith',
+            email: 'jane.smith@example.com',
+            password: 'password2'
+        });
+
+        const user3 = new User({
+            username: 'BobJohnson',
+            email: 'bob.johnson@example.com',
+            password: 'password3'
+        });
+
+        
+        try {
+            // Save the users to the database
+            await User.insertMany([user1, user2, user3]);
+        } catch (error) {
+            console.error("Failed to save users to the database:", error);
+        }
+
+    } catch (error) {
+        console.error("Failed to connect to MongoDB:", error);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
